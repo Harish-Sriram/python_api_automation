c2cbbed9a4849ace02a6ea36bbc8bb8c1fc84a36
Harish Sriram <harish11109026@gmail.com>
2019-07-28 20:49:05 +0530

    28th july - API URL binding done - second commit

--- GET_request/Api_url_binder.py
+++ GET_request/Api_url_binder.py
@@ -6,17 +6,16 @@ _base_url = "https://reqres.in"
 _resource_url = "/api/users?page={}"
 url_template = "{}{}".format(_base_url, _resource_url)
 formatted_url = [url_template.format(i) for i in _ids]
-# response = requests.get(formatted_url[1])
-# responseJson = response.json()
-# print(responseJson)
-# def validate_http_response_code():
-#     assert response.status_code == 200
-#     print("Http status code is 200OK")
-# def validate_page_number():
-#     assert responseJson['page'] == _ids[1]
-#     print("Current page number is: {}".format(_ids[1]))
-# validate_http_response_code()
-#
-# validate_page_number()
+for j in range(0, 5):
+    response = requests.get(formatted_url[j])
+    response_json = response.json()
+    print(response_json)
+    assert response.status_code == 200
+    print("Http status code is 200OK")
+    assert response_json['page'] == _ids[j]
+    print("Current page number is: {}".format(_ids[j]))
+
+
+
 
 
